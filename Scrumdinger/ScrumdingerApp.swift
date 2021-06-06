86ca1411cdc2ce7edda9429ce82e92ff530c14d3
sugitlab <sgmt.snj@gmail.com>
2021-06-06 22:04:33 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Shinji Sugimoto on 2021/06/06.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
