b668cb737367bcf389d9a4901446b57d5c85274f
sugitlab <sgmt.snj@gmail.com>
2021-06-06 23:09:14 +0900

    Creating a CardView: done

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,26 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Shinji Sugimoto on 2021/06/06.
+//
+
+import SwiftUI
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var color: Color
+}
+
+// Test Data
+extension DailyScrum {
+    static var data: [DailyScrum] {
+        [
+            DailyScrum(title: "Design", attendees: ["Cathy", "Daisy", "Simon", "Jonathan"], lengthInMinutes: 10, color: Color("Design")),
+            DailyScrum(title: "App Dev", attendees: ["Katie", "Gray", "Euna", "Luis", "Darla"], lengthInMinutes: 5, color: Color("App Dev")),
+            DailyScrum(title: "Web Dev", attendees: ["Cathy", "Chris", "Christina", "Eden", "Karla", "Lindsey", "Aga", "Chad", "Jenn", "Sarah"], lengthInMinutes: 1, color: Color("Web Dev"))
+        ]
+    }
+}
